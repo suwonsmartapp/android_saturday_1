8e208a099af35531ae30a1650498736c21fbe537
Sang-yeonPark <Bligh-Park@Sang-yeonParkui-MacBook-Pro.local>
2016-08-13 15:42:41 +0900

    LoginActivity basic funciton add

--- app/src/main/java/com/suwonsmartapp/saturdayproject/LoginActivity.java
+++ app/src/main/java/com/suwonsmartapp/saturdayproject/LoginActivity.java
@@ -1,7 +1,36 @@
 package com.suwonsmartapp.saturdayproject;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 /**
  * Created by Bligh-Park on 2016. 8. 13..
  */
-public class LoginActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button init = (Button) findViewById(R.id.init);
+        Button apply = (Button) findViewById(R.id.apply);
+        apply.setOnClickListener(this);
+        init.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.init:
+                Toast.makeText(LoginActivity.this, "초기화 하겠습니다", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.apply:
+                Toast.makeText(LoginActivity.this, "가입합니다", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 }
